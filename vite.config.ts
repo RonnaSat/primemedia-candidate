5d97d65367787953e18f3c072da2bb58a38da37a
RonnaSat <ronnakornniseansut@gmail.com>
2025-07-16 21:23:20 +0700

    update production base path in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/primemedia-candidate-assignment/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/primemedia-candidate/' : '/',
   plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
